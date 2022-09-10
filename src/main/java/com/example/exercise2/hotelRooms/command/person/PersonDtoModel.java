2dbebf092a923a8abb2444ac4a343e75e38c47ff
IvanGit2021 <ivan.dias.moreira@gmail.com>
2022-09-10 23:04:58 +0100

    Add validators

--- src/main/java/com/example/exercise2/hotelRooms/command/person/PersonDtoModel.java
+++ src/main/java/com/example/exercise2/hotelRooms/command/person/PersonDtoModel.java
@@ -5,14 +5,21 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Builder
 @Data
 public class PersonDtoModel {
 
+
     private Integer id;
+    @NotBlank(message = "Name is mandatory")
     private String name;
+    @NotBlank(message = "Age is mandatory")
+    @Min(value = 0, message = "Age must be bigger than 0")
     private Integer age;
+    @NotBlank(message = "City is mandatory")
     private String city;
     private Room room;
 }
--- src/main/java/com/example/exercise2/hotelRooms/command/room/RoomDtoModel.java
+++ src/main/java/com/example/exercise2/hotelRooms/command/room/RoomDtoModel.java
@@ -4,13 +4,18 @@ import com.example.exercise2.hotelRooms.model.Person;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 import java.util.List;
 
 @Builder
 @Data
 public class RoomDtoModel {
 
+    @NotBlank(message = "Room number is mandatory")
+    @Min(value = 0, message = "Room Number must be bigger than 0")
     private Integer roomNumber;
+    @NotBlank(message = "Room must have a hotel name")
     private String hotelName;
     private List<Person> guests;
 }
