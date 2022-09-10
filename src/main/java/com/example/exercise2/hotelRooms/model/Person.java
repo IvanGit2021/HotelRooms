42eeda99cc5a2952d79026415b85f198159b2e85
IvanGit2021 <ivan.dias.moreira@gmail.com>
2022-09-10 17:49:11 +0100

    Fill models

--- src/main/java/com/example/exercise2/hotelRooms/model/Person.java
+++ src/main/java/com/example/exercise2/hotelRooms/model/Person.java
@@ -1,4 +1,27 @@
 package com.example.exercise2.hotelRooms.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Builder
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String city;
+    @ManyToOne
+    private Room room;
+
+    public Person() {
+    }
 }
--- src/main/java/com/example/exercise2/hotelRooms/model/Room.java
+++ src/main/java/com/example/exercise2/hotelRooms/model/Room.java
@@ -1,4 +1,25 @@
 package com.example.exercise2.hotelRooms.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Builder
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private Integer roomNumber;
+    private String hotelName;
+    @OneToMany(mappedBy = "room")
+    private List<Person> guests;
+
+    public Room() {
+    }
 }
