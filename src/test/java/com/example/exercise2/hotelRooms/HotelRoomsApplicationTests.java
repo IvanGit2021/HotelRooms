9d076d6811bcd404129a27ddff9e92d30f3a4156
IvanGit2021 <ivan.dias.moreira@gmail.com>
2022-09-10 17:31:08 +0100

    Initial commit

---
+++ src/test/java/com/example/exercise2/hotelRooms/HotelRoomsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercise2.hotelRooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelRoomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
